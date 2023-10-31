6d9ab456e90ff9376e25a1961651102d4fac0984
mrdjangodev <mirolimcoder@gmail.com>
2023-10-31 17:28:14 +0500

    validation gived to head doctor

--- departments/head_doctor_models.py
+++ departments/head_doctor_models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 # from local
-from hr_management.doctor_models import Doctor
+from hr_management.models import Doctor
+from hr_management.validators import validate_stuff_is_vorking
 from .models import Department
 
 class HeadDoctor(models.Model):
-    doctor = models.OneToOneField(Doctor, on_delete=models.DO_NOTHING)
+    doctor = models.OneToOneField(Doctor, on_delete=models.DO_NOTHING, 
+                                validators=[validate_stuff_is_vorking])
     department = models.OneToOneField(Department, on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True)
 
