a7d67cf876a6f3b1046e41500d6b6db9f84ca4ec
mrdjangodev <mirolimcoder@gmail.com>
2023-11-04 20:09:52 +0500

    save method handled for ATTANDANCE model

--- hr_management/attandance_model.py
+++ hr_management/attandance_model.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 # from .specialization_models import DoctorSpecialization
-from .models import Staff
+from .models import Staff, AvailableTime
+from datetime import datetime
 
 class Attandance(models.Model):
     staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
@@ -12,4 +13,10 @@ class Attandance(models.Model):
     def __str__(self):
         return f"{self.staff} | {self.in_available_day}"
 
+    def save(self, *args, **kwargs):
+        avilable_days = AvailableTime.objects.filter(staff=self.staff).values_list('week_day', flat=True)
+        if datetime.now().weekday() not in avilable_days:
+            self.in_available_day = False
+        super().save(*args, **kwargs)
 
+    
