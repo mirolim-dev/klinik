1ee41bee753f96e62aa4c2c8129c82bbe02003bf
mrdjangodev <mirolimcoder@gmail.com>
2023-11-10 18:36:23 +0500

    Supplier model has been created

--- warehouse/models.py
+++ warehouse/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Supplier(models.Model):
+    name = models.CharField(max_length=255)
+    contact_number = models.CharField(max_length=17)
+    contact_person = models.CharField(max_length=150, help_text="Person's Full name in here")
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def get_all_orders(self):
+        """Returns all orders belonging to Supplier"""
+        return self.order_set.select_related('product')
+
+
 class Section(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
@@ -15,7 +27,7 @@ class ItemCategory(models.Model):
     def __str__(self):
         return self.name
     
-    
+
 class MeasureChoices:
     MILLIGRAMM = 0
     GRAMM = 1
