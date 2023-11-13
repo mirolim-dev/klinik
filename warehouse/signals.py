2620936d84ac13e8d1c065ad0d54c206b3750322
mrdjangodev <mirolimcoder@gmail.com>
2023-11-13 11:56:23 +0500

    signals created for OrderItem and ProductUSage

--- warehouse/signals.py
+++ warehouse/signals.py
@@ -1,5 +1,22 @@
 from django.db.models.signals import pre_save, post_save
-
+from django.dispatch import receiver
 # from local
-from .models import Order, OrderItem, Product
+from .models import Order, OrderItem, Product, ProductUsage
+from .utils import calculate_product_amount
 
+@receiver(post_save, sender=OrderItem)
+def update_product_amount_in_stock(sender, instance, created, **kwargs):
+    """Updates Product's amount in stock after OrderItem added"""
+    product = sender.product
+    product.amount_in_stock, product.measure = calculate_product_amount(product, \
+        sender.amount, sender.measure, True)
+    product.save()
+
+
+@receiver(post_save, sender=ProductUsage)
+def update_product_amount_in_stock(sender, instance, created, **kwargs):
+    """Updates Product's amount in stock after ProductUsage added"""
+    product = sender.product_collections.product
+    product.amount_in_stock, product.measure = calculate_product_amount(product, \
+        sender.product_collections.amount, sender.product_collections.measure, False)
+    product.save()
