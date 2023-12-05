ab2d72c1db63f10e3477596704301c09be02427f
mrdjangodev <mirolimcoder@gmail.com>
2023-12-05 17:01:45 +0500

    searching from departments' main index view done

--- departments/views.py
+++ departments/views.py
@@ -6,12 +6,16 @@ from .models import Department
 
 def main_index(request):
     departments = Department.objects.all()
+    search_data = request.POST.get('search_input')
+    if search_data:
+        departments = Department.objects.filter(name__icontains=search_data)
     paginator = Paginator(departments, 1)  # Show 10 departments per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
     context = {
         'page_obj': page_obj,
+        'search_data': search_data,
     }
   
 
