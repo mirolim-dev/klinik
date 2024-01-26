52ec59d7efbdfacfbf355d58471df7f6d42922a4
mrdjangodev <mirolimcoder@gmail.com>
2024-01-26 20:00:52 +0500

    patients view in department done

--- departments/urls.py
+++ departments/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from .views import (
     main_index, department_detail,
-    main_index2,
+    main_index2, show_all_patients_by_department,
 )
 
 urlpatterns = [
     path('main/', main_index, name='main_index'),
     path('main/2', main_index2, name='main_index2'),
     path('detail/<int:pk>/', department_detail, name='detail-department'),
+    path('<int:department_id>/show_patients/', show_all_patients_by_department, name='show-patients-by-department'),
 ]
--- departments/views.py
+++ departments/views.py
@@ -2,6 +2,7 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from .models import Department
+from hr_management.models import Doctor
 # Create your views here.
 
 def main_index(request):
@@ -9,7 +10,7 @@ def main_index(request):
     search_data = request.POST.get('search_input')
     if search_data:
         departments = Department.objects.filter(name__icontains=search_data)
-    paginator = Paginator(departments, 1)  # Show 10 departments per page
+    paginator = Paginator(departments, 10)  # Show 10 departments per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
@@ -27,6 +28,7 @@ def main_index2(request):
     }
     return render(request, "departments/index3.html", context)
 
+
 def department_detail(request, pk:int):
     department = get_object_or_404(Department, id=pk)
     context = {
@@ -35,3 +37,42 @@ def department_detail(request, pk:int):
     return render(request, 'departments/single_department.html', context)
 
 
+def show_all_patients_by_department(request, department_id:int):
+    department = get_object_or_404(Department, id=department_id)
+    patients = department.get_all_patients()
+    paginator = Paginator(patients, 10)  # Show 10 departments per page
+    page_number = request.GET.get('page')
+    page_patients = paginator.get_page(page_number)
+    # fake_patient_data = [ #it should be removed before deploying
+    #     {
+    #         'id': 1,
+    #         'first_name': "Ibragim",
+    #         'last_name': "Xolmatov",
+    #         'phone': "+998 95 978 45 65",
+    #         'gender': "Male",
+    #         'address': "Qo'qon",
+    #         'date_of_birth': "2001-12-31",
+    #         'insurance_provider': None,
+    #         'insurance_policy_number': None,
+    #     },
+    #     {
+    #         'id': 2,
+    #         'first_name': "Iftixor",
+    #         'last_name': "Xolmatov",
+    #         'phone': "+998 95 978 45 66",
+    #         'gender': "Male",
+    #         'address': "Qo'qon",
+    #         'date_of_birth': "2001-11-15",
+    #         'insurance_provider': None,
+    #         'insurance_policy_number': None,
+    #     },
+    # ]  
+    context = {
+        'department': department,
+        'patients': page_patients,
+    }
+    return render(request, 'departments/show_patients.html', context)
+# def update_doctor_detail(request, doctor_id:int):
+#     doctor = Doctor.objects.get(id=doctor_id)
+#     context = {'doctor': doctor}
+#     return render('departments/update_doctor.html', context)
