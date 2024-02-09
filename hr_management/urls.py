45c0ebb7d9ef11c84e1deace0e2b963761cf0098
mrdjangodev <mirolimcoder@gmail.com>
2024-02-10 01:10:58 +0500

    PatientProfile view've been created

--- hr_management/urls.py
+++ hr_management/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from .views import (
+    patient_profile,
+)
+
 urlpatterns = [
-    
+    path('patient/<int:patient_id>/', patient_profile, name='patient-profile'),
 ]
--- hr_management/views.py
+++ hr_management/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
+from departments.models import (
+    Department,
+)
+from .models import (
+    Patient, 
+    )
 # Create your views here.
+
+def patient_profile(request, patient_id:int):
+    patient = get_object_or_404(Patient, id=patient_id)
+
+    context = {
+        'patient': patient,
+    }
+    return render(request, 'hr_management/patient_profile.html', context)
