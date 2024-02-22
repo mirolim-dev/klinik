5bc668f56b6ed3eb7cb3033651932efcf58b216c
mrdjangodev <mirolimcoder@gmail.com>
2024-02-22 18:26:59 +0500

    updating invoice service status by signal done

---
+++ payments/signals.py
@@ -0,0 +1,34 @@
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+from django.contrib.contenttypes.models import ContentType
+from datetime import datetime, date
+
+# from warehouse.models import ProductUsage, ProductsCollection, Product
+from .models import Invoice, InvoiceService, Payment
+from departments.meals_models import Admission
+
+
+@receiver(post_save, sender=Admission)
+def create_invoice_service_by_admission(sender, instance, **kwargs):
+    if not instance.pk:
+        patient = instance.patient
+        invoice = Invoice.objects.filter(patient=patient, status__in=[1, 2])[0]
+        InvoiceService.objects.create(
+            content_type=ContentType.objects.get_for_model(instance),
+            object_id=instance.id,
+            invoice=invoice,
+            paid=False,
+        )
+
+    
+@receiver(post_save, sender=Payment)
+def update_invoice_service_status(sender, instance, **kwargs):
+    if not instance.pk:
+        invoice_service = instance.invoice_service
+        if instance.amount >= invoice_service.get_total_amount():
+            invoice_service.paid = True
+            invoice_service.save()
+
+
+
+        
