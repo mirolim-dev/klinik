92d8086435e7ca30947cf61cb648c98d13d487a2
mrdjangodev <mirolimcoder@gmail.com>
2024-02-22 18:37:07 +0500

    creating invoiceService by consulting and diagnoz✅

--- payments/signals.py
+++ payments/signals.py
@@ -6,10 +6,22 @@ from datetime import datetime, date
 # from warehouse.models import ProductUsage, ProductsCollection, Product
 from .models import Invoice, InvoiceService, Payment
 from departments.meals_models import Admission
+from events.consulting_models import DiagnozPatientUsage, ConsultingPatientUsage
 
-
-@receiver(post_save, sender=Admission)
-def create_invoice_service_by_admission(sender, instance, **kwargs):
+@receiver(post_save, sender=DiagnozPatientUsage)
+def create_invoice_service_by_diagnoz(sender, instance, **kwargs):
+    if not instance.pk:
+        patient = instance.patient
+        invoice = Invoice.objects.filter(patient=patient, status__in=[1, 2])[0]
+        InvoiceService.objects.create(
+            content_type=ContentType.objects.get_for_model(instance),
+            object_id=instance.id,
+            invoice=invoice,
+            paid=False,
+        )
+    
+@receiver(post_save, sender=ConsultingPatientUsage)
+def create_invoice_service_by_consulting(sender, instance, **kwargs):
     if not instance.pk:
         patient = instance.patient
         invoice = Invoice.objects.filter(patient=patient, status__in=[1, 2])[0]
@@ -20,7 +32,7 @@ def create_invoice_service_by_admission(sender, instance, **kwargs):
             paid=False,
         )
 
-    
+
 @receiver(post_save, sender=Payment)
 def update_invoice_service_status(sender, instance, **kwargs):
     if not instance.pk:
